df974921238e96b79d41ab11232d6ac8d8bee30c
momodou165 <barrymomodouwurry165@gmail.com>
2026-06-21 14:08:45 +0000

    first commit

---
+++ Frondend/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="" data-theme="corporate">
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
+      <h1 className="font-bold text-primary">Hello World</h1>;
+    </div>
+  );
+}
+
+export default App;
